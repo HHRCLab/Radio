99783e7bd0180ff219c8269b10ac10f2b380632d
HHRCLab <HHRCLab@gmail.com>
2023-02-21 15:52:47 +0400

    Connect/disconnect bug fixes.

--- main.py
+++ main.py
@@ -10,31 +10,48 @@ IP = "192.168.1.10"
 PORT = 5050
 arduinolist = [("192.168.1.66", PORT), ("192.168.1.65", PORT)]
 socketlist = []
-bufferSize = 64
-rf = 1
+bufferSize = 128
+rf = [1, 1]
 massage = "a9"
 
 
-udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+def sendmsgagain(tup):
+    socket.socket(socket.AF_INET, socket.SOCK_DGRAM).sendto(massage.encode(encoding="utf=8"), arduinolist[tup])
+    print("resent")
+
+
 for i in range(len(arduinolist)):
     udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     udp.bind((IP, PORT+i+1))
+    udp.settimeout(7)
     socketlist.append(udp)
 
 
 for i in range(len(arduinolist)):
     socketlist[i].sendto(massage.encode(encoding="utf=8"), arduinolist[i])
-    socketlist[i].sendto(massage.encode(encoding="utf=8"), arduinolist[i])
 time.sleep(1)
 
 
-def main1():
+def main1(level):
     global rf
     while True:
         for sock in socketlist:
-            data, addr = sock.recvfrom(bufferSize)
-            print(f"Received data:{list(data)[:6]} from {addr}\n")
-            rf = list(data)[:6]
+            if sock:
+                count = sock.getsockname()[1] % 5051
+                try:
+                    print("in")
+                    data, addr = sock.recvfrom(bufferSize)
+                    temp = list(data)[:6]
+                    rf[count] = round(sum(temp) / len(temp))
+                    print(f"Received data:{rf[count]} from {addr}\n")
+
+                except socket.timeout:
+                    print(f"{sock.getsockname()} timed out")
+                    sendmsgagain(count)
+                    continue
+
+            else:
+                print("f")
 
 
 def web():
@@ -43,14 +60,14 @@ def web():
 
     @app.route("/")
     def home():
-        return render_template('Home.html', rf=rf)
+        return render_template('Home.html', rf=rf, rflen=len(rf))
 
     app.run()
 
 
 if __name__ == "__main__":
     threading.Thread(target=web).start()
-    threading.Thread(target=main1).start()
+    threading.Thread(target=lambda: main1(level=rf)).start()
 
 
 
