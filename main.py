97008cb7a830a355d9e0da696c5f0590890dac1a
linux-lab <hhrclab@gmail.com>
2023-03-22 13:07:12 +0400

    Changed arduino to object

--- main.py
+++ main.py
@@ -16,63 +16,68 @@ massage = "a9"
 green = 12
 yellow = 6
 red = 2
-status = {"0": True, "1": True}
+# status = [x.status for x in socketlist] 
+TimeOut = 7
+
+
+class Arduino:
+    def __init__(self, ip, port):
+        self.IP = ip
+        self.RCVPORT = port
+        self.status = False
+        self.udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.udp.bind((IP, self.RCVPORT))
+        self.udp.settimeout(TimeOut)
+        self.udp.sendto(massage.encode(encoding="utf=8"), (self.IP, PORT))
+        
+
+    def changeStatus(self, state):
+        self.status = state
+        print(f"{self.IP}:{self.status}")
+
+
+    def sendmsg(self):
+        socket.socket(socket.AF_INET, socket.SOCK_DGRAM).sendto(massage.encode(encoding="utf=8"), (self.IP, PORT))
+        print("resent")
 
 
 
 
-def sendmsgagain(tup):
-    x = list(arduinolist.keys())
-    socket.socket(socket.AF_INET, socket.SOCK_DGRAM).sendto(massage.encode(encoding="utf=8"), (x[tup], arduinolist[x[0]][0]))
-    print("resent")
-
-
-for i in arduinolist.values():
-    udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    udp.bind((IP, i[1]))
-    udp.settimeout(7)
-    socketlist.append(udp)
-
-
-for i in range(len(arduinolist)):
-    x = list(arduinolist.keys())
-    socketlist[i].sendto(massage.encode(encoding="utf=8"), (x[i], arduinolist[x[0]][0]))
-time.sleep(1)
-
-def changestatus(tup,state):
-    if state:
-        status.update({f"{tup}": True})
-    else:
-        status.update({f"{tup}": False})
-    print(status)
+for i in range(0, 2):
+    socketlist.append(Arduino(f"192.168.1.6{5+i}", PORT+1+i))
 
 
 def openlogs():
     logs = open("logs.txt", "rt")
     return logs.read()
 
+def status():
+    return str([x.status for x in socketlist])
+
+
 
 def main1(level):
     global rf
     while True:
         for sock in socketlist:
-            if sock:
-                count = sock.getsockname()[1] % 5051
+            if sock.udp:
+                # count = sock.udp.getsockname()[1] % 5051
+                count = socketlist.index(sock)
                 try:
                     print("in")
-                    data, addr = sock.recvfrom(bufferSize)
+                    data, addr = sock.udp.recvfrom(bufferSize)
                     temp = list(data)[:6]
                     rf[count] = round(sum(temp) / len(temp))
                     print(f"Received data:{rf[count]} from {addr}\n")
-                    changestatus(count, True)
+                    
 
                 except socket.timeout:
-                    print(f"{sock.getsockname()} timed out")
-                    changestatus(count, False)
+                    print(f"{sock.udp.getsockname()} timed out")
+                    sock.changeStatus(False)
                     logs = open("logs.txt", "a")
-                    logs.write(f"{time.ctime(1672215379.5045543)}:{sock.getsockname()} is not responding\r")
+                    logs.write(f"{time.ctime(1672215379.5045543)}:{sock.udp.getsockname()} is not responding\r")
                     logs.close()
-                    sendmsgagain(count)
+                    sock.sendmsg()
                     continue
 
             else:
@@ -87,6 +92,10 @@ def web():
     def home():
         return render_template('Home.html', rf=rf, rflen=len(rf), status=status)
 
+    @app.route("/logs")
+    def logs():
+        return render_template("Logs.html", logs=openlogs())
+    
     @app.route("/get_rf", methods=["GET"])
     def get_rf():
         return str(rf)
@@ -94,10 +103,6 @@ def web():
     @app.route("/get_status", methods=["GET"])
     def get_status():
         return str(status)
-
-    @app.route("/logs")
-    def logs():
-        return render_template("Logs.html", logs=openlogs())
 
     app.run()
 
